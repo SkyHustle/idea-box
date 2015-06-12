d85ea0027b383f8f3ad2d5c4f955d4c23f365400
Dmitry Vizersky <dmitry.vizer@gmail.com>
2015-06-12 14:25:22 -0600

    User Registration feature test complete
    - Added show and create actions to user controller
    - Added show view for user

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :users, only: [:new]
+  resources :users, only: [:new, :show, :create]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
