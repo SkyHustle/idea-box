87f49c048c113a094445822ca1d7a236634faf8a
Dmitry Vizersky <dmitry.vizer@gmail.com>
2015-06-12 15:32:43 -0600

    User Login feature test

---
+++ spec/features/users/user_login_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :feature do
+  scenario 'valid user is able to login' do
+    user = User.create(username: "taz", password: "mania")
+
+    visit login_path
+    fill_in "Username", with: user.username
+    fill_in "Password", with: "mania"
+    click_button "Login"
+
+    expect(page).to have_content(user.username)
+  end
+end
