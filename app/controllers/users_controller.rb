0f517214b300c8b32622e2e61d25997efd6c6321
Dmitry Vizersky <dmitry.vizer@gmail.com>
2015-06-12 16:31:14 -0600

    Initiate session upon user registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,12 +4,13 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find_by(id: params[:id])
+    @user = User.find(session[:user_id])
   end
 
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       redirect_to user_path(@user)
     else
       render :new
